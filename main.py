6aa45939e71b34d2ca972575cdb8d0d894d2e0bc
KnotEnvy <jwsnyder@gmail.com>
2023-09-05 20:54:33 -0400

    News API added

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ from transformers import pipeline
 from tkinter import ttk
 
 API_KEY = "YOUR_ALPHA_VANTAGE_API_KEY"
+NEWS_API_KEY = "913bee3728d9419e971abd8ce50c30b5"
 
 # Initialize sentiment analysis pipeline
 sentiment_analyzer = pipeline("sentiment-analysis")
+
+def fetch_news(stock_symbol):
+    url = f"https://newsapi.org/v2/everything?q={stock_symbol}&apiKey={NEWS_API_KEY}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    headlines = [article['title'] for article in data['articles'][:5]]
+    return headlines
 
 def calculate_rsi(data, window):
     delta = data.diff()
@@ -16,22 +24,22 @@ def calculate_rsi(data, window):
     loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
     rs = gain / loss
     rsi = 100 - (100 / (1 + rs))
-    return rsi.iloc[-1]
+    return round(rsi.iloc[-1], 4)
 
 def calculate_ema(data, window):
-    return data.ewm(span=window, adjust=False).mean().iloc[-1]
+    return round(data.ewm(span=window, adjust=False).mean().iloc[-1], 4)
 
 def analyze_sentiment(stock_data, news_headlines):
     df = pd.DataFrame(stock_data).T.astype(float)
     close_prices = df["4. close"]
     
-    moving_average = close_prices.mean()
-    avg_volume = df["5. volume"].mean()
+    moving_average = round(close_prices.mean(), 4)
+    avg_volume = round(df["5. volume"].mean(), 4)
     rsi = calculate_rsi(close_prices, 14)
     ema = calculate_ema(close_prices, 9)
     
     news_sentiment = sentiment_analyzer(news_headlines)
-    avg_sentiment = sum([s['score'] for s in news_sentiment]) / len(news_sentiment)
+    avg_sentiment = round(sum([s['score'] for s in news_sentiment]) / len(news_sentiment), 4)
     
     sentiment = "Undecided"
     confidence = 0.5
@@ -53,8 +61,7 @@ def fetch_stock_data():
     response = requests.get(url)
     data = json.loads(response.text)
     
-    # Placeholder for news headlines, you can replace this with real news API data
-    news_headlines = ["Stock is doing well", "Positive growth", "Market is bullish"]
+    news_headlines = fetch_news(stock_symbol)
     
     if "Time Series (5min)" in data:
         sentiment, confidence, moving_average, avg_volume, avg_sentiment, rsi, ema = analyze_sentiment(data["Time Series (5min)"], news_headlines)
